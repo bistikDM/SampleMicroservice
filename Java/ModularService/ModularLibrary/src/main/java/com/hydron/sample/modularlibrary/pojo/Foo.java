5c08ac082a6ededec5d40b5401763e5fe0d56d01
bistikDM <23036454+bistikDM@users.noreply.github.com>
2023-12-10 20:44:48 -0500

    Added library for reusable code.

---
+++ Java/ModularService/ModularLibrary/src/main/java/com/hydron/sample/modularlibrary/pojo/Foo.java
@@ -0,0 +1,32 @@
+package com.hydron.sample.modularlibrary.pojo;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import java.time.Instant;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@ToString
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Foo implements ExampleData {
+    @JsonProperty
+    private String name;
+
+    @JsonProperty
+    private String identifier;
+
+    @JsonProperty
+    private Instant timestamp;
+
+    @JsonProperty
+    private static final String text = "%s [%s] - Foo says \"Hello, %s!\"";
+
+    @Override
+    public String getMessage() {
+        return String.format(text, this.identifier, this.timestamp, this.name);
+    }
+}
